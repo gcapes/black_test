a007ee0fed25ced48eed39bd641713221b1f9a48
Gerard Capes <gerard.capes@manchester.ac.uk>
2025-03-20 15:26:24 +0000

    Create demo.py

---
+++ .github/workflows/demo.py
@@ -0,0 +1,4 @@
+def my_function(string_input: str): -> str
+  return string_input + string_input + string_input + string_input + string_input + string_input + string_input + string_input + string_input + string_input
+
+print(f'My duplicated string: {my_function("the string ")}')
