b1f220a275cf1d9968e3270deaf67edea973bedb
Gerard Capes <gerard.capes@manchester.ac.uk>
2025-03-20 15:42:31 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,3 +2,5 @@ def my_function(string_input: str) -> str:
   return string_input + string_input + string_input + string_input + string_input + string_input + string_input + string_input + string_input + string_input
 
 print(f'My duplicated string: {my_function("the string ")}')
+
+# Add comment for PR
