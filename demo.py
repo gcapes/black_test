1ea07f55158ab94cc2f9e358e9dc3c4f6efd43bc
Gerard Capes <gerard.capes@manchester.ac.uk>
2025-03-24 11:00:06 +0000

    test push rule

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 def my_function(string_input: str) -> str:
+  """docstring for gh actions test on push"""
   return string_input + string_input + string_input + string_input + string_input + string_input + string_input + string_input + string_input + string_input
 
 print(f'My duplicated string: {my_function("the string ")}')
